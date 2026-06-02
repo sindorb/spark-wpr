1f47224e514910998ab7d93de882ef3a02b42a36
sindorb <ficoff2@gmail.com>
2026-06-02 22:30:26 +0200

    dodane animacije u index

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
 
 document.querySelectorAll(".nav-menu a").forEach(link => {
     link.addEventListener("click", () => {
-        hamburger.classList.remove("active");
         navMenu.classList.remove("active");
     });
 });
